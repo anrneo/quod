cf3a11b7992220bd79c83d58f88d4606d12539cc
Fabian Marin <fabian-guitar@hotmail.com>
2021-03-24 19:33:32 -0500

    commented unused code

--- app/Http/Controllers/HangmanController.php
+++ app/Http/Controllers/HangmanController.php
@@ -16,7 +16,6 @@ class HangmanController extends Controller
 
     public function index()
     {
-        // $letters = explode (',','A,B,C,D,E,F,G,H,I,J,K,L,M,N,Ñ,O,P,Q,R,S,T,U,V,X,Y,Z');
         $caregories = Categorie::All();
         $word = Word::where('categorie_id', $caregories[0]->id)->get()->random();
         return view('welcome',['letters' => $this->letters, 'caregories' => $caregories,  'word' => $word, 'id' => 0]);
@@ -25,7 +24,6 @@ class HangmanController extends Controller
     public function show(Request $request)
     {
         $id = $request->input('category');
-        // $letters = explode (',','A,B,C,D,E,F,G,H,I,J,K,L,M,N,Ñ,O,P,Q,R,S,T,U,V,X,Y,Z');
         $caregories = Categorie::All();
         $word = Word::where('categorie_id', $id)->get()->random();
         return view('welcome',['letters' => $this->letters, 'caregories' => $caregories,  'word' => $word, 'id' => (int)$id]);
